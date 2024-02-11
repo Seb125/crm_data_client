7c853ef3cbae68b2994bd1aa9ba7903b248700c0
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-11 17:32:41 +0100

    not waiting for api

--- netlify/functions/scheduled-function.mjs
+++ netlify/functions/scheduled-function.mjs
@@ -2,8 +2,13 @@ import dataService from "../../src/services/data.service";
 
 
 export default async () => {
-    await dataService.data( {dataToken: "026a65f29080542fed39fb1c9074db7b26699705"})
-    console.log("Data Update Done!")
+    dataService.data( {dataToken: "026a65f29080542fed39fb1c9074db7b26699705"})
+    console.log("Data Update Triggered!")
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Data Update Triggered!" }),
+      };
 }
 
 export const config = {
