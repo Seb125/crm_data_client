910754a8122cd30a30850201084b1ee6d17eb902
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-11 18:27:24 +0100

    kk

--- netlify/functions/scheduled-function.mjs
+++ netlify/functions/scheduled-function.mjs
@@ -5,7 +5,7 @@ export default async () => {
     dataService.data( {dataToken: "026a65f29080542fed39fb1c9074db7b26699705"})
     console.log("Data Update Triggered!")
 
-   
+    return undefined;
 }
 
 export const config = {
