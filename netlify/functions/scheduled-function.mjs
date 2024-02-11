6f4ae287dfcdaba4274301d755863ebe48af1135
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-11 16:33:17 +0100

    FCK

--- netlify/functions/scheduled-function.mjs
+++ netlify/functions/scheduled-function.mjs
@@ -2,7 +2,7 @@ import dataService from "../../src/services/data.service";
 
 
 export default async () => {
-    await dataService.data( {dataToken: import.meta.env.VITE_DATA_TOKEN})
+    await dataService.data( {dataToken: "026a65f29080542fed39fb1c9074db7b26699705"})
     console.log("Data Update Done!")
 }
 
