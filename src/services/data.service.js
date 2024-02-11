6b02f8592b8edd0609c92341ea099c2efd5807ee
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-11 14:43:43 +0100

    kk

--- src/services/data.service.js
+++ src/services/data.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class DataService {
     constructor() {
         this.api = axios.create({
-            baseURL: import.meta.env.VITE_API_URL
+            baseURL: "https://crm-statistics.adaptable.app"
         });
     }
 
