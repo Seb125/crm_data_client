d424709143db879fe410ebfe535590151543adc9
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-11 18:19:10 +0100

    daily

--- netlify/functions/scheduled-function.mjs
+++ netlify/functions/scheduled-function.mjs
@@ -5,12 +5,9 @@ export default async () => {
     dataService.data( {dataToken: "026a65f29080542fed39fb1c9074db7b26699705"})
     console.log("Data Update Triggered!")
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Data Update Triggered!" }),
-      };
+   
 }
 
 export const config = {
-    schedule: "@hourly"
+    schedule: "@daily"
 }
